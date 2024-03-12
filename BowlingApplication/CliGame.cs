49a90185dfa6fb5c6de185895ca9928ca77fccd2
Joakim Vinter-Olesen <joakimviol@hotmail.com>
2024-03-12 15:45:29 +0100

    Error handling

--- BowlingApplication/CliGame.cs
+++ BowlingApplication/CliGame.cs
@@ -6,35 +6,77 @@ public static class CliGame {
     public static void Main(string[] args) {
         Game game = new Game();
         for (int i = 1; i <= 10; i++) {
-            Console.Write($"Frame {i}, first roll:");
-            // TODO: Add error handling for invalid input
-            int firstRoll = Convert.ToInt32(Console.ReadLine());
-            Frame frame = game.AddFrame(firstRoll);
+            Frame? frame = null;
+            while (frame == null) {
+                Console.Write($"Frame {i}, first roll:");
+                frame = TryReadFirstRoll(game);
+            }
 
-            if (firstRoll == 10) {
+            if (frame.IsComplete()) {
                 Console.WriteLine("Strike!");
             } else {
-                Console.Write($"Frame {i}, second roll:");
-                // TODO: Add error handling for invalid input
-                int secondRoll = Convert.ToInt32(Console.ReadLine());
-                frame.SetSecondRoll(secondRoll);
+                do {
+                    Console.Write($"Frame {i}, second roll:");
+                } while (!TryReadSecondRoll(frame));
             }
             PrintScoreSheet(game);
         }
 
         Frame lastFrame = game.Frames.Last();
+        // TODO: move this logic to the Game class
         if (lastFrame.FirstRoll == 10 || lastFrame.FirstRoll + lastFrame.SecondRoll == 10) {
-            Console.Write("Bonus roll:");
-            // TODO: Add error handling for invalid input
-            lastFrame.NextFrame = new Frame(Convert.ToInt32(Console.ReadLine()));
-            if (lastFrame.FirstRoll == 10) {
+            while (lastFrame.NextFrame == null) {
                 Console.Write("Bonus roll:");
-                // TODO: Add error handling for invalid input
-                lastFrame.NextFrame.SetSecondRoll(Convert.ToInt32(Console.ReadLine()));
+                lastFrame.NextFrame = TryReadFirstRoll(game);
+            }
+            if (lastFrame.FirstRoll == 10) {
+                if (lastFrame.NextFrame.FirstRoll == 10) {
+                    while (lastFrame.NextFrame.NextFrame == null) {
+                        Console.Write("Bonus roll:");
+                        lastFrame.NextFrame.NextFrame = TryReadFirstRoll(game);
+                    }
+                } else {
+                    do {
+                        Console.Write("Bonus roll:");
+                    } while (!TryReadSecondRoll(lastFrame.NextFrame));
+                }
             }
             PrintScoreSheet(game);
         }
         Console.WriteLine("Game over!");
+    }
+
+    /**
+     * Tries to read the first roll of a frame from the console.
+     * If the input is invalid, prints an error message and returns null.
+     * If the input is valid, returns the frame.
+     */
+    private static Frame? TryReadFirstRoll(Game game) {
+        try {
+            int firstRoll = Convert.ToInt32(Console.ReadLine());
+            Frame frame = game.AddFrame(firstRoll);
+            return frame;
+        } catch (Exception e) {
+            Console.WriteLine(e.Message);
+            return null;
+        }
+    }
+    
+    /**
+     * Tries to read the second roll of a frame from the console.
+     * If the input is invalid, prints an error message and returns false.
+     * If the input is valid, sets the second roll of the frame and returns true.
+     */
+    private static bool TryReadSecondRoll(Frame frame) {
+        try {
+            int secondRoll = Convert.ToInt32(Console.ReadLine());
+            frame.SetSecondRoll(secondRoll);
+            return true;
+        }
+        catch (Exception e) {
+            Console.WriteLine(e.Message);
+            return false;
+        }
     }
 
     private static void PrintScoreSheet(Game game) {
